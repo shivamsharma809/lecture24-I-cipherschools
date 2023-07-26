193d222dd318590596d4600fac92d93891a286a3
shivamsharma809 <138153296+shivamsharma809@users.noreply.github.com>
2023-07-26 14:41:16 +0530

    Add files via upload

---
+++ Recursion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int fib(int input) // The point at which you want the fibonacci sequence to exist.
+{
+
+// BASE CASE, THAT IS THE POINT WHERE THE RECURSION STOPS, AND ID REALLY IMPORTANT.
+    if(input==0 || input==1)
+    {
+        return 1;
+    }
+
+    int Final_answer;
+
+    Final_answer = fib(input-1) + fib(input-2);
+    return Final_answer;
+}
+
+
+int main()
+{
+    int input;
+    cout<<"enter the number you want to find the fibonicci series to: "<<endl;
+    cin>>input;
+
+    cout<<fib(input);
+
+    return 0;
+
+}
